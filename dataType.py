d624e2b47a4807e74aef74d0ffbc59cc4f2d3b37
robbytrujillo <robbyikusuma88@gmail.com>
2026-09-19 04:44:13 +0700

    Introduction Python Dicoding | Data Type

---
+++ dataType.py
@@ -0,0 +1,41 @@
+x = 6
+print(type(x))
+
+x = "6"
+print(type(x))
+
+x = 1+2j
+print(type(x))
+
+"""
+Output:
+<class ‘int’>
+<class ‘str’>
+<class ‘complex’>
+"""
+
+var = 10
+print(var)
+print(id(var))
+
+var = 11
+print(var)
+print(id(var))
+"""
+Output:
+10
+<memory address>
+11
+<memory address>
+"""
+
+x = True
+print(type(x))
+x = False
+print(type(x))
+
+"""
+Output:
+<class 'bool'>
+<class 'bool'>
+"""
