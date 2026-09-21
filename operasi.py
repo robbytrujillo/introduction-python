3d2973bc673938bb7402dfff84ebed9142c75569
robbytrujillo <robbyikusuma88@gmail.com>
2026-09-21 14:56:07 +0700

    Introduction Python Dicoding | Operasi

---
+++ operasi.py
@@ -0,0 +1,15 @@
+# Operasi pada List, Set, dan String
+# List
+# len()
+
+
+contoh_list = [1, 3, 3, 5, 5, 5, 7, 7, 9]
+
+print(contoh_list)
+print(len(contoh_list))
+
+"""
+Output:
+[1, 3, 3, 5, 5, 5, 7, 7, 9]
+9
+"""
