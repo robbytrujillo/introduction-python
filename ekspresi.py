f36a05d08f86d6f85ac961a30c95f6b2b5310b1c
robbytrujillo <robbyikusuma88@gmail.com>
2026-09-23 10:25:48 +0700

    introduction python | ekspresi

---
+++ ekspresi.py
@@ -0,0 +1,32 @@
+# Ekspresi
+x = 10
+y = 2
+result = x - y
+
+print(result)
+
+"""
+Output:
+8
+"""
+
+angka = [2, 4, 6, 8]
+huruf = ['P', 'Y', 'T', 'H', 'O', 'N']
+gabung = angka + huruf
+
+print(gabung)
+
+"""
+Output:
+[2, 4, 6, 8, 'P', 'Y', 'T', 'H', 'O', 'N']
+"""
+
+learn = ['P', 'Y', 'T', 'H', 'O', 'N']
+replikasi = learn * 2
+
+print(replikasi)
+
+"""
+Output:
+['P', 'Y', 'T', 'H', 'O', 'N', 'P', 'Y', 'T', 'H', 'O', 'N']
+"""
