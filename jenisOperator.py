a53725d0dbfee752e60d52632f91adbf1606b246
robbytrujillo <robbyikusuma88@gmail.com>
2026-09-24 05:08:47 +0700

    introduction python | jenis operator

--- jenisOperator.py
+++ jenisOperator.py
@@ -43,3 +43,118 @@ True
 False
 True
 """
+
+x = "Dicoding"
+y = "Indonesia"
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+"""
+Output:
+
+False
+True
+False
+True
+False
+True
+"""
+
+# Operator Logika
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+
+"""
+Output:
+
+True
+False
+False
+False
+"""
+
+# Operator OR
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+
+"""
+Output:
+
+True
+True
+True
+False
+"""
+
+# Operator NOT
+print(not True)
+print(not False)
+
+"""
+Output:
+
+False
+True
+"""
+
+# Operator Assignment
+# +=
+x = 11
+x += 5
+print(x)
+
+# -=
+x = 11
+x -= 5
+print(x)
+
+# *=
+x = 11
+x *= 5
+print(x)
+
+# /=
+x = 11
+x /= 5
+print(x)
+
+#%=
+x = 11
+x%= 5
+print(x)
+
+
+"""
+Output:
+16
+6
+55
+2.2
+1
+"""
+
+x = 11
+x += 5
+print(x)
+
+
+x = 11
+x = x + 5
+print(x)
+
+"""
+Output:
+16
+16
+"""
