caed99cd66b31ec532e3041f06f8c0cbb8fca579
robbytrujillo <robbyikusuma88@gmail.com>
2026-09-23 15:35:33 +0700

    introduction python | jenis operator

---
+++ jenisOperator.py
@@ -0,0 +1,45 @@
+# Operator Aritmetika
+x = 11
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x // y)
+print(x / y)
+print(x % y)
+print(x ** y)
+
+"""
+Output:
+
+16
+6
+55
+2
+2.2
+1
+161051
+"""
+
+# Operator Relasional
+x = 5
+y = 10
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+"""
+Output:
+
+False
+True
+False
+True
+False
+True
+"""
