a941fcb20590c1e2de5494e8197049b16f7f7af6
robbytrujillo <robbyikusuma88@gmail.com>
2026-09-26 00:38:51 +0700

    introduction python | python Interpreter

---
+++ pythonInterpreter.py
@@ -0,0 +1,36 @@
+for i in range(10):
+    print(i)
+
+"""
+Output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+"""
+
+for j in range(10):
+print(j)
+
+"""
+Output:
+IndentationError: expected an indented block
+"""
+
+teks = "Dicoding"
+Teks = "Indonesia"
+
+print(teks)
+print(Teks)
+
+"""
+Output:
+Dicoding
+Indonesia
+"""
